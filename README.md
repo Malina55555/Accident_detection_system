eba5c3acc23c3b220d793beacd174d136e01c80d
Malina55555 <malinovka55555@yandex.ru>
2026-06-20 17:55:22 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-### Система детекции Дорожно-Транспортных Происшествий 
+# Система детекции Дорожно-Транспортных Происшествий 
+
+Проект разработанной системы детекции и анализа дорожно-транспортных происшествий посредством методов машинного обучения
+
+Подготовлен студентом 4ого курса группы БВТ2203
+
+Несмачной А.А.
+
+## Установка
+
+### 1. Клонирование репозитория
+
+```bash
+git clone <repository-url>
+cd project_translator
+```
+
+### 2. Установка зависимостей
+
+```bash
+pip3 install -r /detector_service/requirements.txt
+pip3 install -r /verification_service/requirements.txt
+pip3 install -r /frontend_service/requirements.txt
+```
+
+### 3. Замена путей к файлам/папкам в следующих файлах:
+
+#### /detector_service/config.py:
+- YOLO_MODEL_PATH
+
+#### /frontend_service/config.py:
+- REPORTS_DIR
+
+#### /verification_service/config.py:
+- RTDETR_MODEL_PATH
+- REPORT_SAVE_PATH
+- FONTS_PATH
+
+## Старт
+
+Переходим по адресу http://127.0.0.1:5000/ в любом браузере
+
+Запускаем автоматическую детекцию с камеры, нажав кнопку "Start Webcum"
+
+либо
+
+Запускаем автоматическую детекцию с видеофайла, выбрав файл, а затем нажав на кнопку "Start Video"
+
+## Предпоказ
+
+
--- verification_service/config.py
+++ verification_service/config.py
@@ -12,17 +12,17 @@ VERIFICATION_PORT = 5002
 # RTDETR для повреждений
 RTDETR_MODEL_PATH = None #заменить на путь к весам модели, н-р: r"C:\Users\User\Diplom\verification_service\models\rtdetr_damage.pt" 
 RTDETR_CONF_THRESHOLD = 0.7
-USE_REAL_RTDETR = False  #True  # False - заглушка, True - реальная модель
+USE_REAL_RTDETR = True  #True  # False - заглушка, True - реальная модель
 
 # VLM1 (Gemma)
 VLM1_MODEL_NAME = "google/gemma-4-e2b-it"
-USE_REAL_VLM1 = False  # False - заглушка, True - реальная модель  Прим-е: заглушка использует реальный вывод модели, но из другого устройства
+USE_REAL_VLM1 = True  # False - заглушка, True - реальная модель  Прим-е: заглушка использует реальный вывод модели, но из другого устройства
 
 # ==========================================================
 # REPORT SETTINGS
 # ==========================================================
 REPORT_SAVE_PATH = None # заменить на директорию с отчтами, н-р: r"C:\Users\User\Diplom\reports" 
-FONTS_PATH = None #хаменить на путь к русскому шрифту, н-р: r"C:\Users\User\Diplom\verification_service\fonts\times.ttf"  # Путь к шрифту для PDF
+FONTS_PATH = None # заменить на путь к русскому шрифту, н-р: r"C:\Users\User\Diplom\verification_service\fonts\times.ttf"  # Путь к шрифту для PDF
 
 # ==========================================================
 # SERVICE URLs
